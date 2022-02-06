8142c070d542426fdaaa67d0a4a727eeaecf7e35
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-02-06 23:17:34 +0700

    create navbar component

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+      <h3>Cooking master</h3>
+    </div>
+  );
+};
+
+export default Navbar;
