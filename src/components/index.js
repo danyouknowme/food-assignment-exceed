332fe1003926feba82028ead4245db7bcad6f76b
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-02-07 02:23:29 +0700

    add export card food detail component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Navbar } from './Navbar/Navbar';
 export { default as Searchbar } from './Searchbar/Searchbar';
 export { default as CardFood } from './CardFood/CardFood';
+export { default as CardFoodDetail } from './CardFoodDetail/CardFoodDetail';
