4b2f1f213b8a2aadb592f7dba9118f03ef7f4df9
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-02-07 02:21:33 +0700

    create food information page

---
+++ src/pages/FoodInfo/FoodInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { CardFoodDetail } from '../../components';
+import './foodinfo.css';
+
+const FoodInfo = () => {
+  return (
+    <div className='card-food-info-container'>
+      <CardFoodDetail />
+    </div>
+  );
+};
+
+export default FoodInfo;
