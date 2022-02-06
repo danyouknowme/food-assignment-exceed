0db5654f0d3abf25048351cc3cc26fcea6037a14
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-02-07 02:28:46 +0700

    render pages

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import { Navbar } from './components';
+import { Index, FoodInfo } from './pages';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Routes>
+        <Route path='/recipes' element={<Index />}></Route>
+        <Route path='/recipes/:recipeId' element={<FoodInfo />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
