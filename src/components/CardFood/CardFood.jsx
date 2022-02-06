f8bc2c360ba9146e71e2722363f0223c47e07e40
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-02-07 00:07:10 +0700

    create cardfood component

---
+++ src/components/CardFood/CardFood.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './cardfood.css';
+
+const CardFood = () => {
+  return (
+    <div className='card-food-container'>
+      <img
+        src='https://bakeitwithlove.com/wp-content/uploads/2021/08/Western-Bacon-Cheeseburger-h.jpg'
+        alt='food'
+      />
+      <h4>Cheeseburger</h4>
+    </div>
+  );
+};
+
+export default CardFood;
