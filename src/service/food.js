1765a6901f26e737c7b7226ce151518169cfdc18
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-02-08 23:16:15 +0700

    remove comments

--- src/service/food.js
+++ src/service/food.js
@@ -6,21 +6,6 @@ export const getMealFromSearching = async (searchValue) => {
 };
 
 export const getMealFromId = async (recipeId) => {
-  const response = await axios
-    .get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
-  // .then((response) => {
-  //   const data = response.data.meals[0];
-  //   setMeal({
-  //     name: data.strMeal,
-  //     img: data.strMealThumb,
-  //   });
-  //   for (const [key, value] of Object.entries(data)) {
-  //     if (key.includes('Ingredient') && value) {
-  //       setIngredients((prev) => [...prev, value]);
-  //     } else if (key.includes('Measure') && value) {
-  //       setMeasure((prev) => [...prev, value]);
-  //     }
-  //   }
-  // });
+  const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${recipeId}`);
   return response.data;
 };
