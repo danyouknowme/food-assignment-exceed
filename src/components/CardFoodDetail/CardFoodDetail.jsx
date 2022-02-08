272cae5f6704b5f98dfa52e6f6b5b76c63ae89dd
Thanathip Suwannakhot <dannajaeiei@hotmail.com>
2022-02-08 23:13:44 +0700

    refactor extract methods

--- src/components/CardFoodDetail/CardFoodDetail.jsx
+++ src/components/CardFoodDetail/CardFoodDetail.jsx
@@ -1,45 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import './card-food-detail.css';
 import { MdCheckBox } from 'react-icons/md';
-import { useParams } from 'react-router-dom';
-import { getMealFromId } from '../../service/food';
 
-const CardFoodDetail = () => {
-  const { recipeId } = useParams();
-  const [meal, setMeal] = useState({ name: '', img: '' });
-  const [ingredients, setIngredients] = useState([]);
-  const [measure, setMeasure] = useState([]);
-
-  const onGetIngredients = (meal) => {
-    for (const [key, value] of Object.entries(meal)) {
-      if (key.includes('Ingredient') && value) {
-        setIngredients((prev) => [...prev, value]);
-      }
-    }
-  };
-
-  const onGetMeasure = (meal) => {
-    for (const [key, value] of Object.entries(meal)) {
-      if (key.includes('Measure') && value) {
-        setMeasure((prev) => [...prev, value]);
-      }
-    }
-  };
-
-  const onGetMealsFromId = (id) => {
-    getMealFromId(id).then((res) => {
-      // console.log(meal, ingredients, measure);
-      const meal = res.meals[0];
-      setMeal({ name: meal.strMeal, img: meal.strMealThumb });
-      onGetIngredients(meal);
-      onGetMeasure(meal);
-    });
-  };
-
-  useEffect(() => {
-    onGetMealsFromId(recipeId);
-  }, [recipeId]);
-
+const CardFoodDetail = ({ meal, ingredients, measure }) => {
   return (
     <div className='card-food-detail-container'>
       <img src={meal.img} alt='food' />
--- src/pages/FoodInfo/FoodInfo.jsx
+++ src/pages/FoodInfo/FoodInfo.jsx
@@ -1,11 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import { CardFoodDetail } from '../../components';
 import './foodinfo.css';
+import { getMealFromId } from '../../service/food';
 
 const FoodInfo = () => {
+  const { recipeId } = useParams();
+  const [meal, setMeal] = useState({ name: '', img: '' });
+  const [ingredients, setIngredients] = useState([]);
+  const [measure, setMeasure] = useState([]);
+
+  const onGetIngredients = (meal) => {
+    for (const [key, value] of Object.entries(meal)) {
+      if (key.includes('Ingredient') && value) {
+        setIngredients((prev) => [...prev, value]);
+      }
+    }
+  };
+
+  const onGetMeasure = (meal) => {
+    for (const [key, value] of Object.entries(meal)) {
+      if (key.includes('Measure') && value) {
+        setMeasure((prev) => [...prev, value]);
+      }
+    }
+  };
+
+  const onGetMealsFromId = (id) => {
+    getMealFromId(id).then((res) => {
+      const meal = res.meals[0];
+      setMeal({ name: meal.strMeal, img: meal.strMealThumb });
+      onGetIngredients(meal);
+      onGetMeasure(meal);
+    });
+  };
+
+  useEffect(() => {
+    onGetMealsFromId(recipeId);
+  }, [recipeId]);
+
   return (
     <div className='card-food-info-container'>
-      <CardFoodDetail />
+      <CardFoodDetail meal={meal} ingredients={ingredients} measure={measure} />
     </div>
   );
 };
